126a07ee9ab493634bc69b93c3b153b91db1ffd8
GuoXiaoBo <ambitiongxb@foxmail.com>
2016-07-29 14:49:23 +0800

    常规

--- Mushrooms/Assets/Script/VLayer/Base/BaseUIHomeView.cs
+++ Mushrooms/Assets/Script/VLayer/Base/BaseUIHomeView.cs
@@ -8,16 +8,21 @@ using HS.Manager;
 
 public class BaseUIHomeView : HS_ViewBase
 {
-	protected UnityEngine.UI.Button _home;
-	protected UnityEngine.UI.Button _watering;
-	protected UnityEngine.UI.Button _strengthen;
-	protected UnityEngine.UI.Button _illustrations;
-	protected UnityEngine.UI.Image _weather;
 	protected UnityEngine.RectTransform _goldInfo;
 	protected UnityEngine.UI.Text _gold;
 	protected UnityEngine.RectTransform _diamondInfo;
 	protected UnityEngine.UI.Text _diamond;
+	protected UnityEngine.UI.Button _watering;
+	protected UnityEngine.UI.Button _home;
+	protected UnityEngine.UI.Button _strengthen;
+	protected UnityEngine.UI.Button _illustrations;
+	protected UnityEngine.UI.Image _weather;
 	protected UnityEngine.UI.Image _humidity;
+	protected UnityEngine.UI.Image _stars1;
+	protected UnityEngine.UI.Image _stars2;
+	protected UnityEngine.UI.Image _stars3;
+	protected UnityEngine.UI.Text _sceneName;
+	protected UnityEngine.UI.Button _strengthenViewClose;
 	
 	internal override GameObject GetViewPrefab()
 	{
@@ -31,20 +36,6 @@ public class BaseUIHomeView : HS_ViewBase
 		Transform transform = this.transform;
 		if (transform == HS_ViewManager.root.transform) return;
 		
-		this._home = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "Home");
-		this.RegisterButtonClickEvent (this._home);
-		
-		this._watering = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "Watering");
-		this.RegisterButtonClickEvent (this._watering);
-		
-		this._strengthen = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "Strengthen");
-		this.RegisterButtonClickEvent (this._strengthen);
-		
-		this._illustrations = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "Illustrations");
-		this.RegisterButtonClickEvent (this._illustrations);
-		
-		this._weather = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "Weather");
-		
 		this._goldInfo = HS_Base.FindProperty<UnityEngine.RectTransform>(transform, "GoldInfo");
 		
 		this._gold = HS_Base.FindProperty<UnityEngine.UI.Text>(transform, "GoldInfo/Gold");
@@ -55,6 +46,32 @@ public class BaseUIHomeView : HS_ViewBase
 		this._diamond = HS_Base.FindProperty<UnityEngine.UI.Text>(transform, "DiamondInfo/Diamond");
 		this._diamond.text = "1234567890";
 		
-		this._humidity = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "Humidity");
+		this._watering = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "HomeView/Watering");
+		this.RegisterButtonClickEvent (this._watering);
+		
+		this._home = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "HomeView/Home");
+		this.RegisterButtonClickEvent (this._home);
+		
+		this._strengthen = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "HomeView/Strengthen");
+		this.RegisterButtonClickEvent (this._strengthen);
+		
+		this._illustrations = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "HomeView/Illustrations");
+		this.RegisterButtonClickEvent (this._illustrations);
+		
+		this._weather = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "HomeView/Weather");
+		
+		this._humidity = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "HomeView/Humidity");
+		
+		this._stars1 = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "StrengthenView/Top/Stars/Stars1");
+		
+		this._stars2 = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "StrengthenView/Top/Stars/Stars2");
+		
+		this._stars3 = HS_Base.FindProperty<UnityEngine.UI.Image>(transform, "StrengthenView/Top/Stars/Stars3");
+		
+		this._sceneName = HS_Base.FindProperty<UnityEngine.UI.Text>(transform, "StrengthenView/Top/Image (2)/SceneName");
+		this._sceneName.text = "名字名字";
+		
+		this._strengthenViewClose = HS_Base.FindProperty<UnityEngine.UI.Button>(transform, "StrengthenView/StrengthenViewClose");
+		this.RegisterButtonClickEvent (this._strengthenViewClose);
 	}
 }
