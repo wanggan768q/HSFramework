d49426c64895d11005f4a14b840943e50cce9732
GuoXiaoBo <ambitiongxb@foxmail.com>
2016-08-01 11:24:09 +0800

    添加蘑菇成长状态机&枯萎状态机

---
+++ Mushrooms/Assets/Script/Sprite/Behaviour/MushroomsRipeStateBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+public class MushroomsRipeStateBehaviour : MonoBehaviour {
+
+
+    public enum RipeState
+    {
+        NULL,
+        Ripe1,
+        Ripe2,
+        Ripe3,
+    }
+
+    private Mushrooms _Mushrooms;
+    private StateMachine<RipeState> _MushroomsRipeState;
+
+    public void Init(Mushrooms m)
+    {
+        this._Mushrooms = m;
+        _MushroomsRipeState = StateMachine<RipeState>.Initialize(this, RipeState.NULL);
+    }
+
+    public void ChangeState(RipeState state)
+    {
+        _MushroomsRipeState.ChangeState(state);
+    }
+
+    public RipeState GetCurState()
+    {
+        return _MushroomsRipeState.State;
+    }
+
+
+    #region Ripe1
+    private void Ripe1_Enter()
+    {
+    }
+
+    private void Ripe1_Update()
+    {
+
+    }
+
+    private void Ripe1_Exit()
+    {
+    }
+    #endregion
+
+
+    #region Ripe2
+    private void Ripe2_Enter()
+    {
+    }
+
+    private void Ripe2_Update()
+    {
+
+    }
+
+    private void Ripe2_Exit()
+    {
+    }
+    #endregion
+
+    #region Ripe3
+    private void Ripe3_Enter()
+    {
+    }
+
+    private void Ripe3_Update()
+    {
+
+    }
+
+    private void Ripe3_Exit()
+    {
+    }
+    #endregion
+
+}
---
+++ Mushrooms/Assets/Script/Sprite/Behaviour/MushroomsWitherStateBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonsterLove.StateMachine;
+
+public class MushroomsWitherStateBehaviour : MonoBehaviour
+{
+    public enum WitherState
+    {
+        NULL,
+        Wither1,
+        Wither2,
+        Wither3,
+    }
+
+    private Mushrooms _Mushrooms;
+    private StateMachine<WitherState> _MushroomsWitherState;
+
+    public void Init(Mushrooms m)
+    {
+        this._Mushrooms = m;
+        _MushroomsWitherState = StateMachine<WitherState>.Initialize(this, WitherState.NULL);
+    }
+
+    public void ChangeState(WitherState state)
+    {
+        _MushroomsWitherState.ChangeState(state);
+    }
+
+    public WitherState GetCurState()
+    {
+        return _MushroomsWitherState.State;
+    }
+
+
+    #region Wither1
+    private void Wither1_Enter()
+    {
+    }
+
+    private void Wither1_Update()
+    {
+        
+    }
+
+    private void Wither1_Exit()
+    {
+    }
+    #endregion
+
+
+    #region Wither2
+    private void Wither2_Enter()
+    {
+    }
+
+    private void Wither2_Update()
+    {
+
+    }
+
+    private void Wither2_Exit()
+    {
+    }
+    #endregion
+
+    #region Wither3
+    private void Wither3_Enter()
+    {
+    }
+
+    private void Wither3_Update()
+    {
+
+    }
+
+    private void Wither3_Exit()
+    {
+    }
+    #endregion
+
+
+
+
+
+}
