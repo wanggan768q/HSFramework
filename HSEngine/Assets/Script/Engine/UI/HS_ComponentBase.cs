ade67a26604399950bd6c6a493a5acbd2fdaf007
GuoXiaoBo <ambitiongxb@foxmail.com>
2016-08-02 11:55:52 +0800

    优化

--- HSEngine/Assets/Script/Engine/UI/HS_ComponentBase.cs
+++ HSEngine/Assets/Script/Engine/UI/HS_ComponentBase.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using HS.Base;
 
 namespace HS.UI
 {
     public abstract class HS_ComponentBase : MonoBehaviour
     {
+        protected HS_Scheduler.Proxy mScheduler;
+
+        public HS_Scheduler.Proxy scheduler
+        {
+            get
+            {
+                if (mScheduler == null)
+                {
+                    mScheduler = new HS_Scheduler.Proxy();
+                }
+                return mScheduler;
+            }
+        }
     }
 
 }
